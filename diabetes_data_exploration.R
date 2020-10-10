e1981fcbed25354c1b1eeb6ef0d45a1938c231cf
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-10-10 14:43:03 -0500

    first commit add script for data exploration

---
+++ diabetes_data_exploration.R
@@ -0,0 +1,23 @@
+################################################################################
+# Análisis exploratorio de la base de datos diabetes.
+# Gonzalo Chacaltana
+###############################################################################
+
+#---------------------------------------------------------
+# Limpiar el workspace
+rm(list = ls())
+
+# Cambiar directorio de trabajo
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+getwd()
+
+# Cargamos la base de datos
+
+library(data.table)
+dataset <-fread("dataDiabetes.csv",header=T, verbose =FALSE, stringsAsFactors=TRUE, showProgress =TRUE)
+
+# Variables y datos informativos de la base de datos
+library(mlr)
+summarizeColumns(dataset)
+
+summary(dataset)
