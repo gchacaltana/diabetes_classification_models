2e319290b19357da132ff8d787f0f33d0b3eeda3
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-12-01 02:04:07 -0500

    add diaberes data part

--- 02_diabetes_data_partition.R
+++ 02_diabetes_data_partition.R
@@ -6,35 +6,105 @@
 ###        pacientes con diabetes
 ###########################################################################
 
-# Cargamos la base de datos
+# Limpiar el entorno de trabajo
+#------------------------------
+rm(list = ls(all.names = TRUE))
+
+# Cambiar el directorio de trabajo
+#---------------------------------
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+# getwd()
+
+# Cargar la base de datos
 library(data.table)
 dataset <- fread("datos/dataDiabetes.csv", header = T, verbose = FALSE, 
                  stringsAsFactors = TRUE, showProgress = TRUE)
 
-# Separamos los conjuntos de entrenamiento (75%) y validación (25%)
+# Cambiamos valores de la target
+dataset$diabetes <- ifelse(dataset$diabetes == "pos", 1, 0)
+
+# load functions.
+source("functions.R")
+
+# Particionar el conjunto de datos. 
+# Entrenamiento (75%) y validación (25%)
 # -----------------------------------------------------------------
+library(ggplot2)
 library(caret)
-set.seed(123)
+set.seed(1212)
 index <- caret::createDataPartition(dataset$diabetes, p = 0.75, list = FALSE)
-dataset_train <- dataset[index, ] # 285 observaciones
-dataset_test <-  dataset[-index, ] # 97 observaciones
+dataset.train <- dataset[index, ] # 285 observaciones
+dataset.test <-  dataset[-index, ] # 97 observaciones
 
-## Distribución variable riesgo - datos de entrenamiento
+## Distribución variable target - datos de entrenamiento
+ggplot(dataset.train, aes(x = factor(diabetes))) +
+    geom_bar() +
+    ggtitle("Distribución variable diabetes \nDatos Entrenamiento") +
+    xlab("Indicador diabetes") + ylab("Pacientes")
 
-plot(dataset_train$diabetes, col=c("green3","firebrick1"), 
-     main = "Distribución variable diabetes - Datos de Entrenamiento",
-     xlab = "Diabetes", ylab = "Pacientes")
+printTable(dataset.train$diabetes)
 
-printTable(dataset_train$diabetes)
+## Distribución variable target - datos de prueba
 
-## Distribución variable riesgo - datos de prueba
+ggplot(dataset.test, aes(x = factor(diabetes))) +
+    geom_bar() +
+    ggtitle("Distribución variable diabetes \nDatos Prueba") +
+    xlab("Indicador diabetes") + ylab("Pacientes")
 
-plot(dataset_test$diabetes, col=c("green3","firebrick1"), 
-     main = "Distribución variable diabetes - Datos de Prueba",
-     xlab = "Diabetes", ylab = "Pacientes")
+printTable(dataset.test$diabetes)
 
-printTable(dataset_test$diabetes)
+# Tranformación de variables cuantitativas (predictoras)
+# ------------------------------------------------------
+
+numeric.variables = c("pregnant","glucose","pressure","triceps","insulin","mass","pedigree","age")
+
+# Transformación por normalización - Entrenamiento
+dataset.train.norm <- dataset.train
+dataset.train.norm[,numeric.variables] <- lapply(dataset.train.norm[,..numeric.variables], normalize_data)
+
+dataset.train.st <- dataset.train
+dataset.train.st[,numeric.variables] <- lapply(dataset.train.st[,..numeric.variables], standardize_data)
+
+# Transformación por normalización - Pruebas
+dataset.test.norm <- dataset.test
+dataset.test.norm[,numeric.variables] <- lapply(dataset.test.norm[,..numeric.variables], normalize_data)
+
+dataset.test.st <- dataset.test
+dataset.test.st[,numeric.variables] <- lapply(dataset.test.st[,..numeric.variables], standardize_data)
+
+
+# Selección de variable - Boruta
+boruta_variables = c("pregnant","glucose","pressure","triceps","insulin","mass","pedigree","age", "diabetes")
+
+dataset.train.boruta.norm <- dataset.train.norm
+dataset.train.boruta.st <- dataset.train.st
+
+dataset.test.boruta.norm <- dataset.test.norm
+dataset.test.boruta.st <- dataset.test.st
+
+
+# Selección de variable - Curva Roc
+roc_variables = c("glucose","triceps","insulin","age", "diabetes")
+
+dataset.train.roc.norm <- dataset.train.norm[, c("glucose","triceps","insulin","age", "diabetes")]
+dataset.train.roc.st  <- dataset.test.st[, c("glucose","triceps","insulin","age", "diabetes")]
+
+dataset.test.roc.norm <- dataset.train.st[, c("glucose","triceps","insulin","age", "diabetes")]
+dataset.test.roc.st  <- dataset.test.st[, c("glucose","triceps","insulin","age", "diabetes")]
+
+save(dataset,
+     # datos.train,
+     dataset.train.boruta.norm,
+     dataset.train.boruta.st,
+     dataset.train.roc.norm,
+     dataset.train.roc.st,
+     dataset.test.boruta.norm,
+     dataset.test.boruta.st,
+     dataset.test.roc.norm,
+     dataset.test.roc.st,
+     file = 'datos/diabetes.rda'
+)
 
 # Creamos los dataset.
-write.csv(dataset_train, "datos/diabetes_train.csv")
-write.csv(dataset_test, "datos/diabetes_test.csv")
+#write.csv(dataset_train, "datos/diabetes_train.csv")
+#write.csv(dataset_test, "datos/diabetes_test.csv")
